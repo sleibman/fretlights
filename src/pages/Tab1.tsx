1f274f28f584fbb04fe1a8bdf0a66c7e1183f0c9
Steve Leibman <sleibman@gmail.com>
2021-03-16 14:34:42 -0400

    wip

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,8 +1,32 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab1.css';
+import { RouteComponentProps } from "react-router";
+import {
+  IonContent,
+  IonHeader,
+  IonFab,
+  IonIcon,
+  IonFabButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonList,
+  IonItem,
+  IonCheckbox,
+  IonNote,
+  IonLabel,
+  IonBadge,
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import "./Tab1.css";
+import { add } from "ionicons/icons";
 
-const Tab1: React.FC = () => {
+import { ScreenOrientation } from "@ionic-native/screen-orientation";
+
+const Tab1: React.FC<RouteComponentProps> = (props) => {
+  console.log("TAB1: " + ScreenOrientation.type);
+  ScreenOrientation.onChange().subscribe(() => {
+    console.log("Orientation Changed in tab1");
+    console.log(ScreenOrientation.type);
+  });
   return (
     <IonPage>
       <IonHeader>
@@ -10,13 +34,25 @@ const Tab1: React.FC = () => {
           <IonTitle>Tab 1</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+      <IonContent>
+        <IonList>
+          <IonItem>
+            <IonCheckbox slot="start" />
+            <IonLabel>
+              <h1>Create Idea</h1>
+              <IonNote>Run Idea by whut whut?</IonNote>
+            </IonLabel>
+            <IonBadge color="success" slot="end">
+              6 Days
+            </IonBadge>
+          </IonItem>
+        </IonList>
+
+        <IonFab vertical="bottom" horizontal="end" slot="fixed">
+          <IonFabButton onClick={() => props.history.push("/new")}>
+            <IonIcon icon={add} />
+          </IonFabButton>
+        </IonFab>
       </IonContent>
     </IonPage>
   );
